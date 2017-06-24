00cbaed1fe521449ea95386bcf452683c9089176
zphuanlove <zphuanlove@gmail.com>
2017-06-24 19:07:04 +0800

    WIFI的动态效果实现

--- app/src/main/java/com/zphuan/animationproject/view/WIFIView.java
+++ app/src/main/java/com/zphuan/animationproject/view/WIFIView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -41,7 +42,15 @@ public class WIFIView extends View {
         paint.setStrokeWidth(6);
         //抗锯齿
         paint.setAntiAlias(true);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                invalidate();
+                handler.postDelayed(this,500);
+            }
+        },500);
     }
+    private Handler handler = new Handler();
 
     /**WIFI控件宽高较小一边的长度*/
     private int wifiLength;
@@ -64,25 +73,33 @@ public class WIFIView extends View {
      */
     private int signalSize = 4;
 
+    /**每次应该绘制的信号个数*/
+    private float shouldExistSignalSize = 0f;
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        shouldExistSignalSize++;
+        if(shouldExistSignalSize>4){
+            shouldExistSignalSize=1;
+        }
         canvas.save();
         //计算最小的扇形信号所在的圆的半径
         float signalRadius = wifiLength/2/signalSize;
         //向下平移画布,保证绘制的图形能够完全显示
         canvas.translate(0,signalRadius);
         for (int i = 0; i < signalSize; i++) {
-            //定义每个信号所在圆的半径
-            float radius = signalRadius * i;
-            RectF rectf = new RectF(radius,radius,wifiLength-radius,wifiLength-radius);
-            if(i<signalSize-1) {
-                paint.setStyle(Paint.Style.STROKE);
-                canvas.drawArc(rectf, startAngle, sweepAngle, false, paint);
-            }else{
-                paint.setStyle(Paint.Style.FILL);
-                canvas.drawArc(rectf, startAngle, sweepAngle, true, paint);
+            if(i>=signalSize-shouldExistSignalSize) {
+                //定义每个信号所在圆的半径
+                float radius = signalRadius * i;
+                RectF rectf = new RectF(radius, radius, wifiLength - radius, wifiLength - radius);
+                if (i < signalSize - 1) {
+                    paint.setStyle(Paint.Style.STROKE);
+                    canvas.drawArc(rectf, startAngle, sweepAngle, false, paint);
+                } else {
+                    paint.setStyle(Paint.Style.FILL);
+                    canvas.drawArc(rectf, startAngle, sweepAngle, true, paint);
+                }
             }
         }
         canvas.restore();
