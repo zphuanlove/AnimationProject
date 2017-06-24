f6318eeea703f2c5e9dda568552b09447b6b686f
zphuanlove <zphuanlove@gmail.com>
2017-06-25 01:37:09 +0800

    添加recycleview，点击弹出对应的自定义dialog

--- app/src/main/java/com/zphuan/animationproject/MainActivity.java
+++ app/src/main/java/com/zphuan/animationproject/MainActivity.java
@@ -1,14 +1,90 @@
 package com.zphuan.animationproject;
 
+import android.app.Dialog;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.zphuan.animationproject.view.MusicView;
+import com.zphuan.animationproject.view.WIFIView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private String[] datas = {"WIFI_Demo","Music_Demo"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recycler_view = (RecyclerView) findViewById(R.id.recycler_view);
+        recycler_view.setLayoutManager(new LinearLayoutManager(this));
+        recycler_view.setAdapter(new RecyclerView.Adapter() {
+                @Override
+                public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                    View view = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+                    MyViewHolder viewHolder = new MyViewHolder(view);
+                    return viewHolder;
+                }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+                MyViewHolder viewHolder = (MyViewHolder) holder;
+                viewHolder.tv.setText(datas[position]);
+                viewHolder.tv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        alertViewDialog(position);
+                    }
+                });
+            }
+
+            @Override
+            public int getItemCount() {
+                return datas.length;
+            }
+
+            class MyViewHolder extends RecyclerView.ViewHolder {
+                private TextView tv;
+
+                public MyViewHolder(View itemView) {
+                    super(itemView);
+                    tv = (TextView) itemView.findViewById(android.R.id.text1);
+                }
+            }
+        });
+    }
+
+    /**
+     * 弹出自定义对话框
+     * @param position
+     */
+    private void alertViewDialog(int position) {
+        View view = null;
+        switch (position){
+            case 0:
+                view = new WIFIView(this);
+                break;
+            case 1:
+                view = new MusicView(this);
+                break;
+        }
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(view);
+        Window window = dialog.getWindow();
+        WindowManager.LayoutParams attributes = window.getAttributes();
+        attributes.width = (int) getResources().getDimension(R.dimen.view_width);
+        attributes.height = (int) getResources().getDimension(R.dimen.view_height);
+        window.setAttributes(attributes);
+        dialog.show();
     }
 }
