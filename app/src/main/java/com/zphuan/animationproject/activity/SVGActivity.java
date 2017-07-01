16712065a0d9c3ed9a085f614633a37a851c9604
zphuanlove <zphuanlove@gmail.com>
2017-07-01 19:32:53 +0800

    通过AnimatedVectorDrawable使两根直线执行动画

--- app/src/main/java/com/zphuan/animationproject/activity/SVGActivity.java
+++ app/src/main/java/com/zphuan/animationproject/activity/SVGActivity.java
@@ -1,8 +1,11 @@
 package com.zphuan.animationproject.activity;
 
 import android.app.Activity;
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.zphuan.animationproject.R;
 
@@ -15,5 +18,13 @@ public class SVGActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_svg);
+        findViewById(R.id.iv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView iv= (ImageView) findViewById(R.id.iv);
+                AnimatedVectorDrawable drawable= (AnimatedVectorDrawable) iv.getDrawable();
+                drawable.start();
+            }
+        });
     }
 }
